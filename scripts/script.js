6ed48ec0e098a032f6d1c4a0919c7c0e2b8bbb5d
juanmfretes <juanmfretes@gmail.com>
2023-03-21 19:45:18 -0300

    chore: add effect of changing the color of the 'rating-items' when click them
    rating-items = icon-round-background + rating-box__rate

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,13 @@
 let finalRating;
 const ratingBtns = Array.from(document.querySelectorAll(".rating-box__rate"));
 const ratingBtnsContainer = document.querySelector(".rating-box__container");
+const ratingBtnsBackground = Array.from(
+  ratingBtnsContainer.querySelectorAll(".icon-round-background")
+);
 console.log(ratingBtns);
+console.log(typeof ratingBtnsBackground);
 
-// Hover a cada rating-box item (aumentar el tamaño del background)
-// Falta agregar un "mouseout" para eliminar el incremento de tamaño
+// Listener for change color while hover (mouseover)
 ratingBtnsContainer.addEventListener("mouseover", function (event) {
   const currentBtn = event.target.closest(".icon-round-background");
   console.log(currentBtn);
@@ -20,6 +23,7 @@ ratingBtnsContainer.addEventListener("mouseover", function (event) {
   numberBtn.classList.add("rating-box__hover");
 });
 
+// Listener for change color while hover (mouseout)
 ratingBtnsContainer.addEventListener("mouseout", function (event) {
   const currentBtn = event.target.closest(".icon-round-background");
   if (!currentBtn) return;
@@ -33,6 +37,27 @@ ratingBtnsContainer.addEventListener("mouseout", function (event) {
 
   console.log(currentBtn);
 });
+
 // Click en un rating-box item (active: cambia de color a orange, luego queda en color gris)
+ratingBtnsContainer.addEventListener("click", function (event) {
+  const currentBtn = event.target.closest(".icon-round-background");
+
+  // remove helper classes from all 'rating-items' different than <<currentBtn>>
+  // OBS: this allow to 'click' in a 'clicked rating item' and remove the helper classes
+  ratingBtnsBackground.forEach((bg) => {
+    if (bg != currentBtn) bg.classList.remove("icon-round-background__clicked");
+  });
+
+  ratingBtns.forEach((bg) => {
+    if (bg != currentBtn) bg.classList.remove("rating-box__clicked");
+  });
+
+  // add helper class to change background color
+  currentBtn.classList.toggle("icon-round-background__clicked");
+
+  // change 'rating item' number color
+  const numberBtn = currentBtn.firstElementChild;
+  numberBtn.classList.toggle("rating-box__clicked");
+});
 
 // Click en submit (active: invertir colores texto y botón)
